970fdf54a58437c7dc80ee6f0991db44926b2099
lityhre <lityhre@gmail.com>
2022-02-11 19:29:31 +0100

    sudoku

---
+++ sudoku/precache-manifest.c634ec6c3dcf686154241988c9c2df69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "599203818b2ae9e7a7d8",
+    "url": "/css/app.d6147687.css"
+  },
+  {
+    "revision": "7f13a64061f4ddf211c6",
+    "url": "/css/chunk-vendors.098a6583.css"
+  },
+  {
+    "revision": "ba66af7cb808e57d088666b4717c2f32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "599203818b2ae9e7a7d8",
+    "url": "/js/app.2cf5c3bc.js"
+  },
+  {
+    "revision": "7f13a64061f4ddf211c6",
+    "url": "/js/chunk-vendors.934e13b0.js"
+  },
+  {
+    "revision": "84eda637ae1bb3bf114f68a136362aa1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
